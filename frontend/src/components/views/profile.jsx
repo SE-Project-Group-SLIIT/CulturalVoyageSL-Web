c86f4c04e1fdbda6f0d922346d6b6e6daf7c7296
OshadhaGeeth <oshadaclans@gmail.com>
2023-05-20 03:08:21 +0530

    profile updating done

--- frontend/src/components/views/profile.jsx
+++ frontend/src/components/views/profile.jsx
@@ -1,24 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
+import { Modal } from 'react-bootstrap';
 import "../views/profile.css";
 import axios from "axios";
-//import UpdateProfile from "./updateProfile";
+import ProfileUpdate from "../views/profileUpdate";
 import { useHistory } from "react-router-dom";
 
 export default function Profile() {
+
+  const [user, setUser] = useState([]);
+  const [id, setId] = useState("");
+
+  const [Name, setName] = useState("");
+  const [Email, setEmail] = useState("");
+  //const [Password, setPassword] = useState("");
+  const [MobileNumber, setMobile] = useState("");
+  const [Location, setLocation] = useState("");
+  const [Bio, setBio] = useState("");
+
+  const [ModalProfileUpdate, setModalProfileUpdate] = useState([]);
+  const [ModalUpdateConfirm, setModalUpdateConfirm] = useState(false);
+
+  let history = useHistory();
+  const win = window.sessionStorage;
+
+  const loadData = () => {
+    // fetch buyer data
+    axios.get(`http://localhost:5000/user/getUser/${Email}`).then((response) => {
+
+        console.log(response.data);
+        console.log(response.data.data[0])
+        let data = {
+            Name :response.data.data[0].Name
+        }
+
+        setUser(response.data.data[0]);
+
+        setName(response.data.data[0].Name);
+        setEmail(response.data.data[0].Email);
+        //setPassword(response.data.data[0].Password);
+        setMobile(response.data.data[0].MobileNumber);
+        setLocation(response.data.data[0].Location);
+        setBio(response.data.data[0].Bio);
+
+        console.log(user);
+    })
+  }
+
+  useEffect(() => {
+    if (win.getItem("Email")) {
+        setEmail(win.getItem("Email"));
+        win.getItem("_id");
+    } if (Email) {
+        loadData();
+        console.log(Email);
+    }
+
+  }, [Email])
+
+  
+  const openmodalUpdate = (data) => {
+    console.log("openModalUpdate", data);
+    setModalProfileUpdate(data);
+    setModalUpdateConfirm(true);
+  }
+
   return (
     
     <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
       <MDBContainer className="py-5 h-100">
         <MDBRow className="justify-content-center align-items-center h-100">
-          <MDBCol lg="6" className="mb-4 mb-lg-0">
+          <MDBCol lg="8" className="mb-4 mb-lg-5">
             <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
               <MDBRow className="g-0">
                 <MDBCol md="4" className="gradient-custom text-center text-white"
                   style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
                   <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
                     alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />
-                  <MDBTypography tag="h5">Oshadha Geeth</MDBTypography>
+                  <MDBTypography tag="h5">{Name}</MDBTypography>
                   <MDBCardText>Tour Guide</MDBCardText>
                   <MDBIcon far icon="edit mb-5" />
                 </MDBCol>
@@ -29,11 +88,11 @@ export default function Profile() {
                     <MDBRow className="pt-1">
                       <MDBCol size="6" className="mb-3">
                         <MDBTypography tag="h6">Email</MDBTypography>
-                        <MDBCardText className="text-muted">oshada@gmail.com</MDBCardText>
+                        <MDBCardText className="text-muted">{Email}</MDBCardText>
                       </MDBCol>
                       <MDBCol size="6" className="mb-3">
                         <MDBTypography tag="h6">Phone</MDBTypography>
-                        <MDBCardText className="text-muted">0773903538</MDBCardText>
+                        <MDBCardText className="text-muted">{MobileNumber}</MDBCardText>
                       </MDBCol>
                     </MDBRow>
 
@@ -41,16 +100,16 @@ export default function Profile() {
                     <MDBRow className="pt-1">
                       <MDBCol size="6" className="mb-3">
                         <MDBTypography tag="h6">Location</MDBTypography>
-                        <MDBCardText className="text-muted">Anuradhapura</MDBCardText>
+                        <MDBCardText className="text-muted">{Location}</MDBCardText>
                       </MDBCol>
                       <MDBCol size="6" className="mb-3">
                         <MDBTypography tag="h6">Bio</MDBTypography>
-                        <MDBCardText className="text-muted">Proffesional Guide Since 2011</MDBCardText>
+                        <MDBCardText className="text-muted">{Bio}</MDBCardText>
                       </MDBCol>
                     </MDBRow>
 
                     <div className="d-flex justify-content-start">
-                      <a href="#" ><MDBBtn color='primary'>Edit</MDBBtn></a>
+                      <MDBBtn color='primary' onClick={() => openmodalUpdate(user)}>Edit</MDBBtn>
                     </div>
                   </MDBCardBody>
                 </MDBCol>
@@ -59,6 +118,22 @@ export default function Profile() {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
+
+
+      <Modal
+        show={ModalUpdateConfirm}
+        onHide={() => setModalUpdateConfirm(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <ProfileUpdate
+          data={ModalProfileUpdate}
+          onHide={() => setModalProfileUpdate(false)}
+        />
+      </Modal>
+
     </section>
+
   );
 }
---
+++ frontend/src/components/views/profileUpdate.js
@@ -0,0 +1,195 @@
+import { React, useEffect, useState } from "react";
+import { Modal, ModalTitle } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import axios from "axios";
+
+export default function UpdateProfile({ data }) {
+    console.log("UpdateProfile", data);
+
+    useEffect(() => {
+        try {
+            console.log("set data>>>>>>", data);
+            setName(data.Name);
+            setEmail(data.Email);
+            setMobile(data.MobileNumber);
+            setPassword(data.Password);
+            setLocation(data.Location);
+            setBio(data.Bio);
+        } catch (err) {
+            window.alert("something went wrong", err);
+        }
+    }, []);
+
+    const [Name, setName] = useState("");
+    const [Email, setEmail] = useState("");
+    const [MobileNumber, setMobile] = useState("");
+    const [Password, setPassword] = useState("");
+    const [Location, setLocation] = useState("");
+    const [Bio, setBio] = useState("");
+
+    function sendData(e) {
+        e.preventDefault();
+
+        console.log("sendData");
+
+        const updateProfile = {
+            Name,
+            Email,
+            MobileNumber,
+            Password,
+            Location,
+            Bio,
+        }
+
+        axios.post(`http://localhost:5000/user/updateUser/${data._id}`, updateProfile).then(() => {
+            console.log(data._id);
+            alert("User Updated")
+            window.location.replace("/profile");
+        }).catch((err) => {
+            console.log("error")
+            alert(err)
+        })
+    }
+
+    return (
+        <div>
+            <Modal.Header closeButton>
+                <Modal.Title>Update Profile: {data.Name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="px-4">
+                <div>
+                    <hr></hr>
+                    <div className="container border-top">
+
+                        <div className="row">
+                            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <form id="contact-form" className="form" role="form" onSubmit={sendData}>
+
+                                    <div class="form-group col-sm">
+                                        <label class="form-label" for="name">Name </label>
+                                        <input
+                                            type="text"
+                                            class="form-control formInput"
+                                            id="name"
+                                            name="name"
+                                            placeholder="Buyer Name"
+                                            tabindex="1"
+                                            required
+                                            onChange={(e) => {
+                                                setName(e.target.value); // assign value
+                                            }}
+                                            value={Name}
+                                        />
+                                    </div>
+
+                                    <div class="form-group col-sm">
+                                        <label class="form-label" for="email">Email </label>
+                                        <input
+                                            type="email"
+                                            class="form-control formInput"
+                                            id="email"
+                                            name="email"
+                                            placeholder="E-mail"
+                                            tabindex="2"
+                                            required
+                                            onChange={(e) => {
+                                                console.log("set email");
+                                                setEmail(e.target.value); // assign value
+                                            }}
+                                            value={Email}
+                                            disabled
+                                        />
+                                    </div>
+
+                                    <div class="form-group col-sm">
+                                        <label class="form-label" for="mobile">Mobile No. </label>
+                                        <input
+                                            type="text"
+                                            class="form-control formInput"
+                                            id="mobile"
+                                            name="mobile"
+                                            placeholder="Mobile No."
+                                            tabindex="3"
+                                            required
+                                            onChange={(e) => {
+                                                setMobile(e.target.value); // assign value
+                                            }}
+                                            value={MobileNumber}
+                                        />
+                                    </div>
+
+                                    <div class="form-group col-sm">
+                                        <label class="form-label" for="password">Password</label>
+                                        <input
+                                            type="text"
+                                            class="form-control formInput"
+                                            id="passwword"
+                                            name="password"
+                                            // placeholder="Enter Strong Password"
+                                            tabindex="4"
+                                            required
+                                            value={Password}
+                                            onChange={(e) => {
+                                                console.log("set password");
+                                                setPassword(e.target.value); // assign value
+                                            }}
+                                            
+                                        />
+                                    </div>
+
+                                    <div class="form-group col-sm">
+                                        <label class="form-label" for="mobile">Location </label>
+                                        <input
+                                            type="text"
+                                            class="form-control formInput"
+                                            id="location"
+                                            name="location"
+                                            placeholder="Location"
+                                            tabindex="5"
+                                            required
+                                            onChange={(e) => {
+                                                setLocation(e.target.value); // assign value
+                                            }}
+                                            value={Location}
+                                        />
+                                    </div>
+
+                                    <div class="form-group col-sm">
+                                        <label class="form-label" for="mobile">Bio </label>
+                                        <input
+                                            type="text"
+                                            class="form-control formInput"
+                                            id="bio"
+                                            name="bio"
+                                            placeholder="Bio"
+                                            tabindex="6"
+                                            required
+                                            onChange={(e) => {
+                                                setBio(e.target.value); // assign value
+                                            }}
+                                            value={Bio}
+                                        />
+                                    </div>
+
+                                    
+                                    <div className="row mt-2 mb-3">
+                                        <div className="col py-3 text-center">
+                                            <button type="submit" className="btn  btn-success">
+                                                Save
+                                            </button>
+                                        </div>
+                                        <div className="col py-3 text-center">
+                                            <button type="reset" className="btn btn-danger">
+                                                Cancel
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal.Body>
+        </div>
+    )
+}
