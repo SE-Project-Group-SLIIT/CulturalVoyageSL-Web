4eecc33b2a0d04493b39b8e5d5533febdd6f7f0b
UdanaIsiwari <it20163372@my.sliit.lk>
2023-05-27 00:20:27 +0530

    last commit

--- frontend/src/components/views/shared/Navbar.js
+++ frontend/src/components/views/shared/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import "./navbar.css";
-import { FaBars, FaRegUser } from 'react-icons/fa'
+import { FaBars, FaRegUser} from 'react-icons/fa'
 import { ImCross } from 'react-icons/im'
 
 export default function NavBar() {
@@ -11,14 +11,17 @@ export default function NavBar() {
     return (
         <nav className='navbar'>
             <div className='logo'>
-            <img src='images/logo.png' alt="Logo" style={{ width: 300, height: 80 }}/>
+            <img src='images/logo.png' alt="Logo"style={{ width: 300, height: 80 }}/>
             </div>
             <ul className={MobileView ? "nav-links-mobile" : "nav-links"} onClick={() => setMobileView(false)}>
-                <Link to="#"><li>Home</li></Link>
+                <Link to="/home"><li>Home</li></Link>
+                <Link to="#"><li>Sites</li></Link>
+                <Link to="/community"><li>Community</li></Link>
+                <Link to="/eventAll"><li>Events</li></Link>
                 <Link to="#"><li>About</li></Link>
-                <Link to="#"><li>Contact</li></Link>
-                <Link to="#"><li>Services</li></Link>
-                <Link to="#"><li><FaRegUser /></li></Link>
+                <Link to="/login"><li>Login</li></Link>
+                <Link to="/register"><li>Register</li></Link>
+                <Link to="/profile"><li><FaRegUser style={{marginTop:35}}/></li></Link>
             </ul>
             <button className='mobile-menu-icon' onClick={() => setMobileView(!MobileView)}>
                 {MobileView ? <ImCross /> : <FaBars />}
